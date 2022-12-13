62d0dacfd101577e3f4a8e6113c7bc6393920876
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2022-12-13 19:27:12 +0700

    Create mylib.h

---
+++ mylib/mylib.h
@@ -0,0 +1,7 @@
+int generateRandomInt(int lower, int upper);
+void initMat(int* mat, int N);
+void initVec(int* vec, int N);
+void printMat(int* mat, int N);
+void printVec(int* vec, int N);
+void matVecMult(int* mat, int* vec, int* vecResult, int N);
+void multiply_vector_matrix(int *mat, int *vec, int N, int *vector_expected);
